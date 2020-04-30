2c53804124ad9318f1be00da74f071fd7ad9fef1
awrznc <49797644+awrznc@users.noreply.github.com>
2020-04-30 22:30:58 +0900

    targetディレクトリが作成されていない場合でもビルドできるように修正

--- src/build.rs
+++ src/build.rs
@@ -5,6 +5,7 @@ fn main() {
     let json = include_str!("./../docs/assets/json/characters.json");
     let data_string = ["json_parse!(", json,")"].concat();
     let data_bytes = data_string.as_bytes();
+    fs::create_dir_all("target").unwrap();
     let mut file = fs::File::create("./target/converted_data.rs").unwrap();
     file.write(data_bytes).unwrap();
 }

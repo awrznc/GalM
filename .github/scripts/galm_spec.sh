e41099f4c711641cf50b5240befea19ea0b8bc80
awrznc <49797644+awrznc@users.noreply.github.com>
2020-05-05 19:53:57 +0900

    galmコマンドで文字列リストをソートできるように修正

--- .github/scripts/galm_spec.sh
+++ .github/scripts/galm_spec.sh
@@ -3,20 +3,33 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 GALM_DIR=${SCRIPT_DIR}/../../target/release/examples
 
-echo 'galm "王様レストラン" --dictionary "皇様レストラン,玉様レストラン,大様レストラン"'
-result=`${GALM_DIR}/galm "王様レストラン" --dictionary "皇様レストラン,玉様レストラン,大様レストラン"`
+echo 'galm --version'
+result=`${GALM_DIR}/galm --version`
 
-if [ "${result}" = "玉様レストラン" ]; then
+if [[ "${result}" =~ ^(galm version ([0-9]+\.).{3})$ ]]; then
     echo -e "\e[32m    => '${result}' ok! \e[m"
 else
     echo -e "\e[31m    => '${result}' error. \e[m"
     exit 1
 fi
 
-echo 'galm --version'
-result=`${GALM_DIR}/galm --version`
 
-if [[ "${result}" =~ ^(galm version ([0-9]+\.).{3})$ ]]; then
+echo 'galm "王様" --file ${SCRIPT_DIR}/data.txt'
+result=`${GALM_DIR}/galm "王様" --file ${SCRIPT_DIR}/data.txt`
+expect=`echo -e "玉様\n皇様\n大様"`
+
+if [ "${result}" = "${expect}" ]; then
+    echo -e "\e[32m    => '${result}' ok! \e[m"
+else
+    echo -e "\e[31m    => '${result}' error. \e[m"
+    exit 1
+fi
+
+echo 'echo -e "皇様\n玉様\n大様" | ${GALM_DIR}/galm "王様"'
+result=`echo -e "皇様\n玉様\n大様" | ${GALM_DIR}/galm "王様"`
+expect=`echo -e "玉様\n皇様\n大様"`
+
+if [ "${result}" = "${expect}" ]; then
     echo -e "\e[32m    => '${result}' ok! \e[m"
 else
     echo -e "\e[31m    => '${result}' error. \e[m"
--- examples/galm.rs
+++ examples/galm.rs
@@ -5,59 +5,18 @@ const VERSIONSTR: &'static str = env!("CARGO_PKG_VERSION");
 
 // --help で表示する文言
 const HELPSTR: &'static str = r"Usage:
-    galm [-d | --dictionary (<file_path> | <string>)]
+    galm [-f | --file (<file_path> | <string>)]
          [-h | --help] [-v | --version]
 Options:
-    -d --dictionary     dictionary file_path or string.
+    -f --file           file path.
     -h --help           Show this message.
     -v --version        Show version number.";
-
-// Extend of Levenshtein distance
-fn get_levenshtein_distance(str1: &str, str2: &str) -> usize {
-
-    // initialize galm
-    let galm = galm::new();
-
-    // initialize table
-    let table_x_size = str1.chars().count() + 1;
-    let table_y_size = str2.chars().count() + 1;
-    let mut table = vec![0; table_x_size * table_y_size];
-    for i in 0..table_x_size {
-        table[i] = i * galm.max_distance_size;
-    }
-    for i in 0..table_y_size {
-        table[i*(table_x_size)] = i * galm.max_distance_size;
-    }
-
-    // テーブルを埋める
-    for i in 1..table_y_size {
-        for j in 1..table_x_size {
-
-            // 比較値の用意
-            let up          = table[j+((i-1)*table_x_size)  ] + galm.max_distance_size;
-            let left        = table[j+(  i  *table_x_size)-1] + galm.max_distance_size;
-            let upper_left  = {
-                let char1 = str1.chars().nth(j-1).unwrap();
-                let char2 = str2.chars().nth(i-1).unwrap();
-                let c = if char1 == char2 { 0 } else { galm.get_distance( &char1.to_string(), &char2.to_string() ) as usize };
-                table[j+((i-1)*table_x_size)-1] + c
-            };
-
-            // 最小値を求める
-            table[j+(i*table_x_size)] = std::cmp::min(std::cmp::min(up, left), upper_left);
-        }
-    }
-
-    // テーブルの右下（配列の最後）の値を返す
-    return table[(table_x_size*table_y_size)-1 as usize];
-}
-
 
 // 似ているパラメータ名を表示する
 fn get_similar_param(input_param: &str) -> &str {
     use galm::search::Iterator;
     return [
-        "-d", "--dictionary",
+        "-f", "--file",
         "-h", "--help",
         "-v", "--version"
     ].iter().get_similar_word(input_param);
@@ -89,34 +48,77 @@ fn main() {
         }
     }
 
-    // 引数が指定されていない場合は --help を出力
-    if args.len() >= 1 && args.len() < 3 {
-        println!("{}", HELPSTR);
-        return ();
+    
+    if args.len() == 1 {
+        
+    }
+
+    match args.len() {
+        1 => {
+            // 引数が指定されていない場合は --help を出力
+            println!("{}", HELPSTR);
+            return ();
+        },
+        2 => {
+
+            use std::io::{self, Read};
+            let mut buffer = String::new();
+            let stdin = io::stdin();
+            let mut handle = stdin.lock();
+            let _ = handle.read_to_string(&mut buffer);
+
+            // initialize galm
+            let galm = galm::new();
+
+            // sort
+            let sort_key = &*args[1];
+            let mut vec = buffer.lines().collect::<Vec<&str>>();
+            vec.sort_by_key( |candidate| galm.get_word_distance(sort_key, candidate) );
+            
+            // print
+            for word in vec {
+                println!("{}", word);
+            }
+            return ();
+        },
+        _ => {}
     }
 
     // 各処理
     match &*args[2] {
-        "-d" | "--dictionary" => {
+        "-f" | "--file" => {
             if args.len() > 3 {
-                let max_distance = usize::max_value();
-                let input_param = &*args[1];
-                let result = &*args[3].split(",").fold(("", max_distance), |most_similar_param, param| {
-                        let distance = get_levenshtein_distance(input_param, param);
-                        if distance < most_similar_param.1 { (param, distance) } else { most_similar_param }
-                }).0;
-                println!("{}", result);
+
+                use std::fs::File;
+                use std::io::prelude::*;
+                let mut file = File::open(&*args[3]).unwrap();
+                let mut buffer = String::new();
+                let _ = file.read_to_string(&mut buffer);
+
+                // initialize galm
+                let galm = galm::new();
+
+                // sort
+                let sort_key = &*args[1];
+                let mut vec = buffer.lines().collect::<Vec<&str>>();
+                vec.sort_by_key( |candidate| galm.get_word_distance(sort_key, candidate) );
+                
+                // print
+                for word in vec {
+                    println!("{}", word);
+                }
             } else {
-                println!("Error");
+                println!(r"Error:
+    filepath is not specified.");
             }
         },
-        _ => {
+        arg => {
             println!(r"Error:
     not exists option: `{}`
     most similar param name: `{}`
     try 'galm --help' for more information",
-                &*args[1],
-                get_similar_param(&*args[1]));
+                arg,
+                get_similar_param(arg));
         }
     }
 }

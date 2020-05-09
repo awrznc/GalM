3a9456b67a480c352a54fd3d96e2952a040447b2
awrznc <49797644+awrznc@users.noreply.github.com>
2020-05-09 13:05:10 +0900

    特徴量の情報を保持するファイルをJSONからCSVに変更

--- src/lib.rs
+++ src/lib.rs
@@ -71,142 +71,45 @@
 //!
 
 
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
-
-/// Character
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub struct Character<'a> {
-    pub name: &'a str,
-    pub cost: usize,
-}
-
-impl Ord for Character<'_> {
-    fn cmp(&self, other: &Character) -> Ordering {
-        other.cost.cmp(&self.cost).then_with(|| self.name.cmp(&other.name))
-    }
-}
-
-impl PartialOrd for Character<'_> {
-    fn partial_cmp(&self, other: &Character) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn get_low_cost<'character>(
-    adjacent_list: &'character Characters,
-    start: &'character str,
-    goal: &str
-) -> Option<usize> {
-
-    // set route info
-    let mut hash: std::collections::HashMap<&str, usize> = std::collections::HashMap::new();
-    for key in adjacent_list.keys() { hash.insert(key, usize::MAX); }
-
-    // set start
-    let mut heap = BinaryHeap::new();
-    heap.push(Character { cost: 0, name: start });
-    hash.insert(start, 0);
-
-    // search shortest distance
-    while let Some(Character { cost, name }) = heap.pop() {
-        // println!("total_cost: {}, name: {}", cost, name);
-
-        // 最短経路を見つけることができたら終了
-        if name == goal { return Some(cost); }
-
-        // すでにより良い経路を見つけていたらスキップ
-        if cost > hash[name] { continue; }
-
-        match &adjacent_list.get(name) {
-            None => continue,
-            Some(adjacent) => {
-                for point in adjacent.iter() {
-                    let next_point = Character { cost: cost + point.cost, name: point.name };
-                    if next_point.cost < hash[next_point.name] {
-                        heap.push(next_point);
-                        hash.insert(next_point.name, next_point.cost);
-                    }
-                }
-            }
-        }
-    }
-    None
+// #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[derive(Debug)]
+pub struct Characters {
+    pub names: Vec<&'static str>,
+    pub costs: Vec<usize>,
 }
 
 
-macro_rules! load_json {
+macro_rules! load_csv {
     ($path:tt) => {
         {
-            let json_string: Vec<&str> = include_str!($path).split('"').collect();
-
-            let mut strings: Vec<&str> = Vec::new();
-            for (i, val) in json_string.iter().enumerate() {
-                if (i + 1) % 2 == 0 {
-                    strings.push(val);
-                }
-            }
-
-            let mut characters: Characters = std::collections::HashMap::new();
-            let mut hash_key: &str = "";
-            let mut node: &str = "";
-            let mut cost: usize = 0;
-            let node_str: &str = "node";
-            let cost_str: &str = "cost";
-            let mut is_key: bool = true;
-            let mut is_align: (bool, bool) = (false, false);
-            let mut is_node: bool = false;
-            let mut is_cost: bool = false;
-            let mut char_vec: Vec<Character> = Vec::new();
-
-            for string in strings.iter() {
-                if is_key == true {
-                    is_key = false;
-                    hash_key = string;
-                } else if string == &node_str {
-                    is_node = true;
-                } else if string == &cost_str {
-                    is_cost = true;
-                } else if is_node == true {
-                    node = string;
-                    is_align.0 = true;
-                    is_node = false;
-                } else if is_cost == true {
-                    cost = string.parse::<usize>().unwrap();
-                    is_align.1 = true;
-                    is_cost = false;
+            let mut characters: Vec<&str> = Vec::new();
+            let costs: Vec<usize> = include_str!(
+                $path
+            ).lines().enumerate().filter_map( |(line_index, line_value)| {
+                if line_index != 0 {
+                    Some(
+                        line_value.split(',').map( |comma_value| {
+                            comma_value.parse::<usize>().unwrap()
+                        }).collect::<Vec<usize>>()
+                    )
                 } else {
-                    characters.insert(hash_key, char_vec.clone());
-                    char_vec = Vec::new();
-                    hash_key = string;
-                    continue;
+                    characters = line_value.split(',').collect();
+                    None
                 }
-                if is_align == (true, true) {
-                    char_vec.push(Character { name: node, cost: cost });
-                    is_align = (false, false);
-                }
-            }
+            }).into_iter().flatten().collect();
 
-            characters.insert(hash_key, char_vec.clone());
-
-            characters
+            Characters { names: characters, costs: costs }
         }
     };
 }
 
 
 /// Database
+#[derive(Debug)]
 pub struct Database {
     pub characters: Characters,
     pub max_distance_size: usize
-    // pub Idioms: Idioms
 }
-
-/// Characters
-pub type Characters = std::collections::HashMap<
-    &'static str,
-    std::vec::Vec<Character<'static>>
->;
 
 /// Database
 impl Database {
@@ -218,8 +121,8 @@ impl Database {
     /// ```
     pub fn new() -> Database {
         return Database {
-            characters: load_json!("./../docs/assets/json/characters.json"),
-            max_distance_size: 100
+            characters: load_csv!("./../docs/assets/csv/characters.csv"),
+            max_distance_size: 255
         };
     }
 
@@ -237,10 +140,17 @@ impl Database {
     /// assert_eq!(distance, 30);
     /// ```
     pub fn get_distance(&self, from: &str, to: &str) -> u8 {
-        return match get_low_cost(&self.characters, from, to) {
-            None => self.max_distance_size as u8,
-            Some(i) if self.max_distance_size < i => { i as u8 },
-            Some(i) => i as u8,
+        let index_x = match self.characters.names.iter().position(|&r| r == from) {
+            Some(i) => i,
+            None => return self.max_distance_size  as u8
+        };
+        let index_y = match self.characters.names.iter().position(|&r| r == to) {
+            Some(i) => i,
+            None => return self.max_distance_size  as u8
+        };
+        return match self.characters.costs[index_y + ( index_x * self.characters.names.len() )] {
+            i if self.max_distance_size < i => { self.max_distance_size as u8 },
+            i => i as u8,
         };
     }
 

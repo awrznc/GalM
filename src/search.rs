8b2929ecf27f4b6aa3a551ae34b071e8ee4b66e0
awrznc@gmail.com <awrznc@gmail.com>
2025-04-05 18:28:31 +0900

    cargo fmt

--- src/search.rs
+++ src/search.rs
@@ -11,7 +11,6 @@ pub trait Iterator<'a, 'b> {
 
 /// Extend Iterator for search similar word.
 impl<'a, 'b> Iterator<'a, 'b> for std::slice::Iter<'a, &str> {
-
     type Item = &'a str;
 
     /// Get similar word.
@@ -32,10 +31,14 @@ impl<'a, 'b> Iterator<'a, 'b> for std::slice::Iter<'a, &str> {
         let galm = Database::default();
         self.fold(("", usize::max_value()), |most_similar_param, param| {
             let distance = galm.get_word_distance(input_param, param);
-            if distance < most_similar_param.1 { (param, distance) } else { most_similar_param }
-        }).0
+            if distance < most_similar_param.1 {
+                (param, distance)
+            } else {
+                most_similar_param
+            }
+        })
+        .0
     }
-
 
     /// Partial match and sort.
     ///
@@ -51,10 +54,11 @@ impl<'a, 'b> Iterator<'a, 'b> for std::slice::Iter<'a, &str> {
     /// assert_eq!(vec!["nana", "banana"], result);
     /// ```
     fn partial_match(self, matching_string: &'b str) -> Vec<&'a str> {
-        let mut result: Vec<&'a str> = self.filter(|string| {
-            string.contains(matching_string)
-        }).cloned().collect();
-        result.sort_by_key( |string| string.chars().count() );
+        let mut result: Vec<&'a str> = self
+            .filter(|string| string.contains(matching_string))
+            .cloned()
+            .collect();
+        result.sort_by_key(|string| string.chars().count());
         result
     }
 }
